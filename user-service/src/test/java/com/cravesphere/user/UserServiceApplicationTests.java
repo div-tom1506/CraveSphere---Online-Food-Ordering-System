0e5fa10eae1c14f4c3645685dcf7bb248f1b2bfa
div-tom1506 <divyanshuk1506@gmail.com>
2025-03-01 22:44:49 +0530

    initial commit

---
+++ user-service/src/test/java/com/cravesphere/user/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cravesphere.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
